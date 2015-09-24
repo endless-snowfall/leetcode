aad9b7fc38b272c8360352c84acdbf113f6fda72
endless.snowfall <endless.snowfall@gmail.com>
2015-09-23 23:01:18 -0400

    Initial stuff.

---
+++ src/leetcode/problems/Problem001.java
@@ -0,0 +1,14 @@
+package leetcode.problems;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class Problem001 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
