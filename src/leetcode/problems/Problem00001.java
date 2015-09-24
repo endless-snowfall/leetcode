533737b69b6a67a1039233d26efa667733cef1bd
endless.snowfall <endless.snowfall@gmail.com>
2015-09-23 23:25:19 -0400

    Renaming file.

---
+++ src/leetcode/problems/Problem00001.java
@@ -0,0 +1,13 @@
+package leetcode.problems;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class Problem00001 {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+}
