61d211234149e36970740a17c02743a4ab410872
endless.snowfall <endless.snowfall@gmail.com>
2015-09-26 19:15:13 -0400

    Adding Problem 6.

---
+++ src/leetcode/problems/problem00006/Solution.java
@@ -0,0 +1,34 @@
+package leetcode.problems.problem00006;
+
+public class Solution {
+
+    public int reverse(int x) {
+        if (x < 10 && x > -10) {
+            return x;
+        }
+
+        boolean isNegative = false;
+        if (x < 0) {
+            isNegative = true;
+            x *= -1;
+        }
+
+        int result = 0;
+
+        while (x > 0) {
+            int digit = x % 10;
+            x /= 10;
+
+            if (Integer.MAX_VALUE / 10 < result) {
+                return 0;
+            }
+
+            result *= 10;
+            result += digit;
+        }
+
+        return (isNegative)
+            ? result *= -1
+            : result;
+    }
+}
---
+++ tst/leetcode/problems/problem00006/SolutionTest.java
@@ -0,0 +1,53 @@
+package leetcode.problems.problem00006;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import leetcode.problems.common.TestBase;
+
+public class SolutionTest extends TestBase {
+
+    private Solution solution;
+
+    @Before
+    public void before() {
+        solution = new Solution();
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(321, solution.reverse(123));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(-123, solution.reverse(-321));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(0, solution.reverse(0));
+    }
+
+    @Test
+    public void test4() {
+        assertEquals(5, solution.reverse(5));
+    }
+
+    @Test
+    public void test5() {
+        assertEquals(-5, solution.reverse(-5));
+    }
+
+    @Test
+    public void test6() {
+        assertEquals(Integer.MIN_VALUE, solution.reverse(-1999999999));
+    }
+
+    @Test
+    public void test7() {
+        assertEquals(Integer.MAX_VALUE, solution.reverse(1999999999));
+    }
+}
