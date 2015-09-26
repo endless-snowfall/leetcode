44fbe320ab8fb08d54bbbd341e588ec553e7fe64
endless.snowfall <endless.snowfall@gmail.com>
2015-09-26 18:35:20 -0400

    Adding Problem 5.

---
+++ src/leetcode/problems/problem00005/Solution.java
@@ -0,0 +1,45 @@
+package leetcode.problems.problem00005;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Solution {
+
+    public String convert(String s, int numRows) {
+        if (s == null || s.isEmpty() || numRows < 1) {
+            return "";
+        } else if (numRows == 1 | numRows > s.length()) {
+            return s;
+        }
+
+        int[] rowAssignment = new int[s.length()];
+        rowAssignment[0] = 0;
+        boolean increasingDirection = true;
+
+        for (int i = 1; i < s.length(); i++) {
+            int previousRowAssignment = rowAssignment[i - 1];
+
+            if (previousRowAssignment == 0) {
+                increasingDirection = true;
+            } else if (previousRowAssignment == numRows - 1) {
+                increasingDirection = false;
+            }
+
+            rowAssignment[i] = (increasingDirection)
+                    ? previousRowAssignment + 1
+                    : previousRowAssignment - 1;
+        }
+
+        StringBuilder[] builders = new StringBuilder[numRows];
+        IntStream.range(0, numRows).forEach(i -> builders[i] = new StringBuilder());
+
+        for (int i = 0; i < rowAssignment.length; i++) {
+            builders[rowAssignment[i]].append(s.charAt(i));
+        }
+
+        StringBuilder resultBuilder = new StringBuilder();
+        Arrays.stream(builders).forEachOrdered(builder -> resultBuilder.append(builder));
+
+        return resultBuilder.toString();
+    }
+}
---
+++ tst/leetcode/problems/problem00005/SolutionTest.java
@@ -0,0 +1,48 @@
+package leetcode.problems.problem00005;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import leetcode.problems.common.TestBase;
+
+public class SolutionTest extends TestBase {
+
+    private Solution solution;
+
+    @Before
+    public void before() {
+        solution = new Solution();
+    }
+
+    @Test
+    public void test1() {
+        assertEquals("PAHNAPLSIIGYIR", solution.convert("PAYPALISHIRING", 3));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals("", solution.convert(null, 3));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals("", solution.convert("", 3));
+    }
+
+    @Test
+    public void test4() {
+        assertEquals("a", solution.convert("a", 100));
+    }
+
+    @Test
+    public void test5() {
+        assertEquals("abcdefg", solution.convert("abcdefg", 100));
+    }
+
+    @Test
+    public void test6() {
+        assertEquals("AB", solution.convert("AB", 1));
+    }
+}
